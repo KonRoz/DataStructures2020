600ea7e9616280a89420d44d30bcd96fda391bb1
Konrad Rozanski <krozansk@nd.edu>
2020-07-12 21:17:57 -0400

    adding Lab 3-1

---
+++ Lab3-2/VideoGame.h
@@ -0,0 +1,87 @@
+/*
+author: Konrad Rozanski
+file: VideoGame.h
+classes: VideoGame
+
+This is the header file for for the VideoGame class.
+
+It contains several private members describing a typical
+video game sold by retail outlets. There are accessors 
+and mutators associated with every private member. Likewise,
+there are 6 constructors included --> one default constructor,
+one constructor which takes 7 arguments.
+
+This file is generally similar to that of Lab 1. That said, a 
+public static member variable called num_games was added in order 
+to keep track of the number of instances of the VideoGame class.
+
+showGame is overloaded. When passed no arguments, the values of all 
+7 private members are displayed. When two arguments are passed (i.e.
+two filters), the private members to which the two filters correspond
+are printed to the console.
+
+salePrice calculates the sale price of the video game 
+based upon the percentage that it is passed. It displays
+it and then stores it as the new price of the video game. 
+
+showGameCentury uses the value of releaseYear to display
+which century the game was developed in.
+*/
+
+#ifndef VIDEOGAMES_H
+#define VIDEOGAMES_H
+#include <string>
+
+using namespace std;
+
+class VideoGame
+{
+
+	private:
+
+		char console;
+		double price;
+		string title;
+		int releaseYear;
+		string developer;
+		string genre;
+		bool isAgeRestricted;
+
+	public:
+        // public static members
+        static unsigned int num_games;
+
+		// constructors
+		VideoGame();
+		VideoGame(char con);
+		VideoGame(char con, double pric);
+		VideoGame(char con, double pric, string tit);
+		VideoGame(char con, double pric, string tit, int relyear);
+		VideoGame(char con, double pric, string tit, int relyear, string dev, string gen, bool ar);
+
+		// accessors 
+		char getConsole() { return console; }
+		double getPrice() { return price; }
+		string getTitle() { return title; }
+		int getReleaseYear() { return releaseYear; }
+		string getDeveloper() { return developer; }
+		string getGenre() { return genre; }
+		bool getIsAgeRestricted() { return isAgeRestricted; }
+
+		// mutators
+		void setConsole(char c) { console=c; }
+		void setPrice(double p) { price=p; }
+		void setTitle(string t) { title=t; }
+		void setReleaseYear(int y) { releaseYear=y; }
+		void setDeveloper(string d) { developer=d; }
+		void setGenre(string g) { genre=g; }
+		void setIsAgeRestricted(bool a) { isAgeRestricted=a; }
+
+		// additional methods
+		void showGame();
+        void showGame(string filter1, string filter2);
+		void salePrice(double percentOff);
+		void showGameCentury();	
+};
+
+#endif
---
+++ Lab3-2/VideoGameCollection.h
@@ -0,0 +1,88 @@
+/*
+author: Konrad Rozanski
+file: VideoGameCollection.h
+classes: VideoGameCollection
+
+This is the header file for VideoGameCollection class.
+
+It contains three private members and the associated public 
+members. The first is a struct which sets the structure for each
+node within the linked list, VideoGameCollection. The last two 
+private members, head and tail, are pointers to the first and
+last element in the linked list respectively. A single default 
+constructor is also included which sets the head and tail members
+to null. A destructor is defined to delete the linked list elements
+as they are dynamically allocated using new.
+
+createNode creates a node within the linked list using the instance
+of VideoGame that it is passed. It tacks this new node onto
+the end of the linked list.
+
+addVideoGameToEnd collects information from the user to create 
+a new instance of VideoGame and passes it to createNode.
+
+deleteVideoGameFromEnd deletes the element at the end of the 
+linked list. 
+
+loadDataAutomatically takes the passed CSV or XML file (located in
+the same working directory) and parses it line by line to create 
+the desired VideoGame object. These objects are then passed to 
+createNode one by one.
+
+printReport allows the user to print out a description of every
+VideoGame object within the linked list. This is done by
+utilizing the showGame method that comes packaged in each 
+VideoGame object.
+
+modifyProductField allows the user to select a given product (via index)
+and change a specified filed within it.
+
+generateNewCSV allows the user to save the changes made over the course of the
+run cycle in the same CSV that was used to initially populate several VideoGame
+instances. The old CSV contents are overwritten when this occurs.
+
+printFilteredReport allows the user to print a report of the VideoGame instances 
+within VideoGameCollection using two specified fields.
+*/
+
+#ifndef VIDEOGAMESCOLLECTION_H
+#define VIDEOGAMESCOLLECTION_H
+#include <string>
+#include "VideoGame.h"
+
+using namespace std; 
+
+class VideoGameCollection
+{
+    private:
+
+        struct VGNode
+        {
+            VGNode* next;
+            VideoGame videogame;
+            int nodeIndex;
+        };
+
+        VGNode* head;
+        VGNode* tail;
+
+    public:
+
+        // Constructor
+        VideoGameCollection();
+
+        // Destructor
+        ~VideoGameCollection();
+
+        // Additional Methods
+        void createNode (const VideoGame &video_game);
+        void addVideoGameToEnd();
+        void deleteVideoGameFromEnd();
+        void loadDataAutomatically(const string &file_name);
+        void printReport();
+        void modifyProductField();
+        void generateNewCSV(const string &file_name);
+        void printFilteredReport();
+};
+
+#endif
