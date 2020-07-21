1cc86a1a32bea1fec91856664478cae0436860a0
Konrad Rozanski <krozansk@nd.edu>
2020-07-21 02:46:11 -0500

    finishing Lab 3.3

---
+++ Lab3-3/VideoGameCollection.cpp
@@ -0,0 +1,425 @@
+/*
+author: Konrad Rozanski
+file: VideoGameCollection.cpp
+
+This is the implementation file for VideoGameCollection.h
+*/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "VideoGameCollection.h"
+
+using namespace std;
+
+VideoGameCollection::VideoGameCollection()
+{
+    head = NULL;
+    tail = NULL;
+}
+
+VideoGameCollection::~VideoGameCollection()
+{
+    VGNode* current = head;
+
+    while (current != NULL)
+    {
+        VGNode* next = current->next;
+        delete current;
+        current = next;
+    }
+
+    head = NULL;
+}
+
+void VideoGameCollection::createNode (const VideoGame &video_game)
+{
+    // This might have to be new VGNode() with no parens
+    VGNode* temp = new VGNode;
+    temp->videogame = video_game;
+    temp->next = NULL;
+    temp->nodeIndex = VideoGame::num_games;
+
+    if (head == NULL)
+    {
+        head = temp;
+        tail = temp;
+        temp = NULL;
+    }
+    else
+    {
+        tail->next = temp;
+        tail = temp;
+    }
+    VideoGame::num_games++;
+}
+
+void VideoGameCollection::addVideoGameToEnd()
+{
+    char console;
+    double price;
+    string title;
+    int releaseYear;
+    string developer;
+    string genre;
+    bool isAgeRestricted;
+
+    cout << "\n\nEnter Console --> x(box), p(lay station), P(C), w(ii), g(ame boy) : ";
+    cin >> console;
+
+    cout << "Enter Price : ";
+    cin >> price;
+
+    cout << "Enter Title : ";
+    cin >> title;
+
+    cout << "Enter Release Year : ";
+    cin >> releaseYear;
+
+    cout << "Enter Developer : ";
+    cin >> developer;
+
+    cout << "Enter Genre : ";
+    cin >> genre;
+
+    cout << "Enter Age Restriction --> 1(true), 0(false) : ";
+    cin >> isAgeRestricted;
+
+    VideoGame video_game (console, price, title, releaseYear, developer, genre, isAgeRestricted);
+    createNode(video_game);
+}
+
+void VideoGameCollection::deleteVideoGameFromEnd()
+{
+    if (head == NULL)
+    {
+        cout << "The list is empty\n";
+    }
+    else if (head->next == NULL)
+    {
+        delete head;
+    } 
+    else 
+    {
+        VGNode* second_last = head;
+
+        while (second_last->next->next != NULL)
+        {
+            second_last = second_last->next;
+        }
+
+        delete (second_last->next);
+
+        second_last->next = NULL;
+        tail = second_last;
+    }
+    VideoGame::num_games--;
+}
+
+void VideoGameCollection::loadDataAutomatically(const string &file_name)
+{
+    fstream fileIn;
+    fileIn.open(file_name, ios::in);
+
+    vector<string> row;
+    string line;
+    string word;
+
+    while (getline(fileIn, line))
+    {
+        row.clear();
+
+        istringstream s (line);
+
+        while (getline(s, word, ','))
+        {
+            row.push_back(word);
+        }
+
+        VideoGame aVideoGame(row[0][0], stod(row[1]), row[2], stoi(row[3]), row[4], row[5], (row[6].compare("true") == 0) ? true : false);
+
+        createNode(aVideoGame);
+
+        cout << "LINE WAS READ : (" << line << ")"  << endl;
+    }
+    if (fileIn.bad())
+    {
+        cout << "An IO error occured\n";
+    } 
+    else if (!fileIn.eof())
+    {
+        cout << "A format error occured\n";
+    } 
+    else
+    {
+        cout << endl << "Records Read!" << endl;
+    }
+    fileIn.close();
+}
+
+void VideoGameCollection::printReport()
+{
+    // Using the showGame method that was implemented in Lab 1
+
+    cout << "These are the games available for purchase : " << endl;
+    cout << " - - - - - - - - - - - - - - " << endl;
+
+    VGNode* temp = new VGNode;
+    temp = head;
+
+    while (temp != NULL)
+    {
+        cout << to_string(temp->nodeIndex) << "\n\n";
+        temp->videogame.showGame();
+        temp = temp->next;
+    }
+}
+
+void VideoGameCollection::modifyProductField()
+{
+    int gameToBeModified;
+    string fieldToBeModified;
+
+    VGNode* temp = new VGNode;
+    temp = head;
+
+    cout << "Input Game Index : ";
+    cin >> gameToBeModified;
+
+    cout << "\nInput Game Field (Title, Price, Console, Release, Developer, Genre, Restriction) : ";
+    cin >> fieldToBeModified;
+
+    while (temp->nodeIndex != gameToBeModified)
+    {
+        temp = temp->next;
+    }
+
+    if ("Title" == fieldToBeModified)
+    {
+        string title;
+        cout << "Enter New Title : ";
+        cin >> title;
+        temp->videogame.setTitle(title);
+    } else if ("Price" == fieldToBeModified)
+    {
+        int price;
+        cout << "Enter New Price : ";
+        cin >> price;
+        temp->videogame.setPrice(price);
+    } else if ("Console" == fieldToBeModified)
+    {
+        char console;
+        cout << "Enter New Console : ";
+        cin >> console;
+        temp->videogame.setConsole(console);
+    } else if ("Release" == fieldToBeModified)
+    {
+        int yearOfRelease;
+        cout << "Enter New Year of Release : ";
+        cin >> yearOfRelease;
+        temp->videogame.setReleaseYear(yearOfRelease);
+    } else if ("Developer" == fieldToBeModified)
+    {
+        string developer;
+        cout << "Enter New Developer : ";
+        cin >> developer;
+        temp->videogame.setDeveloper(developer);
+    } else if ("Genre" == fieldToBeModified)
+    {
+        string genre;
+        cout << "Enter New Genre : ";
+        cin >> genre;
+        temp->videogame.setGenre(genre);
+    } else if ("Restriction" == fieldToBeModified)
+    {
+        bool ageRestricted;
+        cout << "Enter New Age Restriction : ";
+        cin >> ageRestricted;
+        temp->videogame.setIsAgeRestricted(ageRestricted);
+    } else
+    {
+        cout << "\nPlease Input Valid Field Name\n";
+    }
+
+    cout << "\n\nModified Game\n\n";
+    temp->videogame.showGame();
+}
+
+void VideoGameCollection::generateNewCSV(const string &file_name)
+{
+    ofstream myfile;
+    myfile.open(file_name);
+
+    VGNode* temp = new VGNode;
+    temp = head;
+
+    while (temp != NULL)
+    {
+        string title = temp->videogame.getTitle();
+        string price = to_string(temp->videogame.getPrice());
+        string console(1, temp->videogame.getConsole());
+        string release = to_string(temp->videogame.getReleaseYear());
+        string developer = temp->videogame.getDeveloper();
+        string genre = temp->videogame.getGenre();
+        string restriction = temp->videogame.getIsAgeRestricted() ? "true" : "false";
+        string the_line = "" + console + "," + price + "," + title + "," + release + "," + developer + "," + genre + "," + restriction + "\n";
+
+        myfile << the_line;
+
+        temp = temp->next;
+    }
+    myfile.close();
+}
+
+void VideoGameCollection::printFilteredReport()
+{
+    string filter1;
+    string filter2;
+
+    VGNode* temp = new VGNode;
+    temp = head;
+
+    cout << "\nField Filters :  Title, Price, Console, Release, Developer, Genre, Restriction\n";
+
+    cout << "Input First Filter : ";
+    cin >> filter1;
+
+    cout << "Input Second Filter : ";
+    cin >> filter2;
+
+    while (temp != NULL)
+    {
+        cout << to_string(temp->nodeIndex) << "\n\n";
+        temp->videogame.showGame(filter1, filter2);
+        temp = temp->next;
+    }
+}
+
+void VideoGameCollection::partition(VGNode* headl, VGNode** front, VGNode** back)
+{
+    VGNode* fast;
+    VGNode* slow;
+
+    if (headl == NULL || headl->next == NULL)
+    {
+        *front = headl;
+        *back = NULL;
+    } else
+    {
+        slow = headl;
+        fast = headl->next;
+
+        while(fast != NULL)
+        {
+            fast = fast->next;
+
+            if (fast != NULL)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        *front = headl;
+        *back = slow->next;
+        slow->next = NULL;
+    }
+}
+
+VideoGameCollection::VGNode* VideoGameCollection::mergeListsPrice(VGNode* a, VGNode* b)
+{
+    VGNode* mergedList = NULL;
+
+    if (a == NULL)
+    {
+        return b;
+    } else if (b == NULL)
+    {
+        return a;
+    }
+
+    if (a->videogame.getPrice() <= b->videogame.getPrice())
+    {
+        mergedList = a;
+        mergedList->next = mergeListsPrice(a->next, b);
+    } else
+    {
+        mergedList = b;
+        mergedList->next = mergeListsPrice(a, b->next);
+    }
+    return mergedList;
+}
+
+VideoGameCollection::VGNode* VideoGameCollection::mergeListsTitle(VGNode* a, VGNode* b)
+{
+    VGNode* mergedList = NULL;
+
+    if (a == NULL)
+    {
+        return b;
+    } else if (b == NULL)
+    {
+        return a;
+    }
+
+    if (a->videogame.getTitle().compare(b->videogame.getTitle()) < 0)
+    {
+        mergedList = a;
+        mergedList->next = mergeListsTitle(a->next, b);
+    } else
+    {
+        mergedList = b;
+        mergedList->next = mergeListsTitle(a, b->next);
+    }
+    return mergedList;
+}
+
+void VideoGameCollection::mergeSortPrice(VGNode** source)
+{
+    VGNode* head = *source;
+    VGNode* a = NULL;
+    VGNode* b = NULL;
+
+    if (head == NULL || head->next == NULL)
+    {
+        return;
+    }
+
+    partition(head, &a, &b);
+
+    mergeSortPrice(&a);
+    mergeSortPrice(&b);
+
+    *source = mergeListsPrice(a, b);
+}
+
+void VideoGameCollection::mergeSortTitle(VGNode** source)
+{
+    VGNode* head = *source;
+    VGNode* a = NULL;
+    VGNode* b = NULL;
+
+    if (head == NULL || head->next == NULL)
+    {
+        return;
+    }
+
+    partition(head, &a, &b);
+
+    mergeSortTitle(&a);
+    mergeSortTitle(&b);
+
+    *source = mergeListsTitle(a, b);
+}
+
+void VideoGameCollection::generateTitleSortedList()
+{
+    mergeSortTitle(&head);
+}
+
+void VideoGameCollection::generatePriceSortedList()
+{
+    mergeSortPrice(&head);
+}
---
+++ Lab3-3/main.cpp
@@ -0,0 +1,92 @@
+/*
+author: Konrad Rozanski
+file: main.py
+
+This file creates the menu through which the user can access the 
+functionality of the VideoGameCollection class.
+
+The user can use each of the seven given options.
+
+The program asks the user to choose another option after one
+is selected.
+
+An unspecified option cannot be chosen --> the program will just ask
+for a legitamite option.
+*/
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <climits>
+#include "VideoGame.h"
+#include "VideoGameCollection.h"
+
+using namespace std;
+
+int main()
+{
+    //instantiating the container class
+    VideoGameCollection myVideoGameCollection;
+
+    string fileName;
+
+    cout << "Welcome to the Video Game Collection Program \n\n";
+    char selected;
+
+    cout << "XML or CSV File Name : ";
+    cin >> fileName;
+    myVideoGameCollection.loadDataAutomatically(fileName);
+
+    while (selected != '9')
+    {
+        cout << "\n\n\t\tVideo Games Menu\n\n";
+        cout << "Items : " << VideoGame::num_games << "\n";
+
+        cout << "1. Display Complete List of Video Games\n";
+        cout << "2. Add a new Video Game To End\n";
+        cout << "3. Delete a Video Game From End\n";
+        cout << "4. Modify Product Field\n";
+        cout << "5. Create Updated CSV\n";
+        cout << "6. Generate Report (2 Field Filter)\n";
+        cout << "7. Generate Title Sorted List\n";
+        cout << "8. Generate Price Sorted List\n";
+        cout << "9. Exit Program\n";
+
+        cout << "Select Value : "; 
+        cin >> selected;
+
+        switch (selected)
+        {
+            case '1':   myVideoGameCollection.printReport();
+                        break;
+
+            case '2':   myVideoGameCollection.addVideoGameToEnd();
+                        break;
+
+            case '3':   myVideoGameCollection.deleteVideoGameFromEnd();
+                        break;
+
+            case '4':   myVideoGameCollection.modifyProductField();
+                        break;
+
+            case '5':   myVideoGameCollection.generateNewCSV(fileName);
+                        break;
+
+            case '6':   myVideoGameCollection.printFilteredReport();
+                        break;
+
+            case '7':   myVideoGameCollection.generateTitleSortedList();
+                        break;
+
+            case '8':   myVideoGameCollection.generatePriceSortedList();
+                        break;
+
+            case '9':   break;
+
+            default:    cout << "\nPLEASE SELECT A VALID OPTION!\n" << endl;
+        }
+        cin.clear();
+        cin.ignore(INT_MAX, '\n');
+    }
+    return 0;
+}
